99428efe252aa624b1fbe0e00d34a07abeeea1b7
moto710 <thien.nhm92@gmail.com>
2022-10-12 11:17:06 +0700

    variables

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        byte i = 10;
+        float f = 20.5f;
+        double d = 20.5d;
+        boolean b = true;
+        char c = 'a';
+        String s = "Ha Noi";
+        System.out.println(i);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(s);
+    }
+}
